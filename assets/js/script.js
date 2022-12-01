a5e65672990996638ce590b3c1b1472122f14090
Murenga Idrissa <idrissam@afriticgroup.com>
2022-12-01 15:01:29 -0400

    changed some codes on javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,24 @@ const text4 = document.getElementById("text4")
 
 document.addEventListener("click", (e)=>{
 
-    if (e.target.nodeName === "H6") {
+    if(e.target.nodeName === "H6"){
 
-        
-        if (e.target.dataset.type === "section7-h1") {
-
-            text1.classList.toggle("section7-p1")
-        }
-
-        if (e.target.dataset.type === "section7-h2") {
-
-            text2.classList.toggle("section7-p1")
-        }
-
-        if (e.target.dataset.type === "section7-h3") {
-
-            text3.classList.toggle("section7-p1")
-        }
-
-        if (e.target.dataset.type === "section7-h4") {
-
-            text4.classList.toggle("section7-p1")
+        switch (e.target.nodeName === "H6") {
+    
+            case e.target.dataset.type === "section7-h1":
+                text1.classList.toggle("section7-p1")
+                break;
+    
+            case e.target.dataset.type === "section7-h2":
+                text2.classList.toggle("section7-p1")
+                break;
+    
+            case e.target.dataset.type === "section7-h3" :
+                text3.classList.toggle("section7-p1")
+                break;
+            case e.target.dataset.type === "section7-h4":
+                text4.classList.toggle("section7-p1")
+                break;    
         }
     }
 })
